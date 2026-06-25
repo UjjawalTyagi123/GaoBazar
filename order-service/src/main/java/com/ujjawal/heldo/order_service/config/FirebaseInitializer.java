e144531035d359c0df4b464a8e048911747380b8
Ujjawal Tyagi <tyagiujjawal763@gmail.com>
2026-06-25 12:19:21 +0530

    Added temporary check for config variable

--- order-service/src/main/java/com/ujjawal/heldo/order_service/config/FirebaseInitializer.java
+++ order-service/src/main/java/com/ujjawal/heldo/order_service/config/FirebaseInitializer.java
@@ -13,12 +13,18 @@ import java.nio.charset.StandardCharsets;
 @Component
 public class FirebaseInitializer {
 
-    @Value("${FIREBASE_SERVICE_ACCOUNT}")
+    @Value("${FIREBASE_SERVICE_ACCOUNT:NOT_FOUND}")
     private String firebaseConfig;
 
     @PostConstruct
     public void initialize() {
-
+        System.out.println(
+                "Firebase variable starts with: " +
+                        firebaseConfig.substring(
+                                0,
+                                Math.min(20, firebaseConfig.length())
+                        )
+        );
         try {
 
             FirebaseOptions options =
